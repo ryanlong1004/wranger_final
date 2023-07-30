4fd6133d59ac5c96c13e496e8aef8223e81f3f34
Ryan Long <ryanlong1004@gmail.com>
2023-07-30 22:13:02 +0000

    Debugging

--- wrangler.py
+++ wrangler.py
@@ -58,7 +58,7 @@ class CLI:
             logger.warning("'%s' directory does not exist... creating", output_path)
             output_path.mkdir(parents=True, exist_ok=True)
 
-    def get_queue(self, _paths: list[pathlib.Path]) -> Generator[Path, None, None]:
+    def get_queue(self, _paths) -> Generator[Path, None, None]:
         """creates generator of files for parsing"""
         for _path in _paths:
             if _path.is_dir():
